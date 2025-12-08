f7f50f98d4baf776532a8c1de9e8759071e99982
Tim De Jong <gitlab@thinkpractice.nl>
2025-12-08 11:08:52 +0100

    HTTPURLResponse on Linux is made available via FoundationNetworking. Added platform conditional import for Linux

--- Sources/HuggingFace/Hub/Pagination.swift
+++ Sources/HuggingFace/Hub/Pagination.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+    import FoundationNetworking
+#endif
 
 /// Sort direction for list queries.
 public enum SortDirection: Int, Hashable, Sendable {
